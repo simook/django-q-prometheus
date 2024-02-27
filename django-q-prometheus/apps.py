07f7b1bdffe89cfe310bfeed934b7b27caea2a52
Kyle Simukka <simook@gmail.com>
2024-02-27 08:54:32 +0100

    [wip] initial django app for exporting django-q metrics.

---
+++ django-q-prometheus/apps.py
@@ -0,0 +1,62 @@
+import time
+from . import Metrics
+from django.db.models.signals import post_save
+from django.apps import AppConfig
+from django.dispatch import receiver
+from django_q.models import Task
+from prometheus_client import Counter, Gauge, Summary
+
+TASKS_SUCCESS = Gauge('django_q_tasks_success', 
+    'A count of successful tasks.')
+TASKS_FAILED = Gauge('django_q_tasks_failed',
+    'A count of failed tasks.')
+TASKS_QUEUED = Gauge('django_q_tasks_queued',
+    'A count of queued tasks.')
+SCHEDULES_COUNT = Gauge('django_q_schedules_count', 
+    'A count of schedules.')
+CLUSTER_COUNT = Gauge('django_q_cluster_count',
+    'A count of clsuters.')
+WORKER_COUNT = Gauge('django_q_worker_count',
+    'A count of workers.')
+REINCARNATION_COUNT = Gauge('django_q_reincarnation_count', 
+    'A count of processes that have been reincarnated.')
+AVERAGE_EXEC_TIME = Summary('django_q_average_execution_seconds',
+    'The average execution time in the last 24 hours.')
+TASKS_SUCCESS_PER_DAY = Summary('django_q_tasks_per_day',
+    'The count of sucessful tasks in the last 24 hours')
+
+last_called = None
+
+def call_hook(sender, **kwargs):
+    if last_called is None:
+        last_called = time.time()
+    # only collect metrics at maxiumum once every 30 seconds.
+    if time.time() - last_called < 30:
+        return
+    
+    m = Metrics()
+    TASKS_SUCCESS.set(m.success_count)
+    TASKS_FAILED.set(m.failure_count)
+    TASKS_QUEUED.set(m.queue_count)
+    SCHEDULES_COUNT.set(m.schedule_count)
+    CLUSTER_COUNT.set(m.cluster_count)
+    WORKER_COUNT.set(m.worker_count)
+    REINCARNATION_COUNT.set(m.reincarnation_count)
+
+    exec, tasks = m.average_execution_time
+    AVERAGE_EXEC_TIME.set(exec)
+    TASKS_SUCCESS_PER_DAY.set(tasks)
+    
+    last_called = time.time()
+
+class DjangoQConfig(AppConfig):
+    name = "django_q_proemtheus"
+
+    def ready(self):
+        """ Inject the signals 
+        """
+        from django_q.signals import pre_execute, pre_enqueue
+        pre_enqueue.connect(call_hook)
+        pre_execute.connect(call_hook)
+
+
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='django-q-prometheus',
+    packages=['django-q-prometheus'],
+    description='django-q performance metrics',
+    version='1.0.0',
+    url='http://github.com/simooka/django-q-prometheus',
+    author='Kyle Simukka',
+    author_email='kylesimukka@gmail.com',
+    keywords=['django-q','prometheus','metrics'],
+    install_requires=[
+        'django',
+        'django-q',
+        'prometheus-client',
+    ],
+)
