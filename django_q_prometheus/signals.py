01d0902962e62c1a87a97306f51430dceabe5957
Kyle Simukka <simook@gmail.com>
2024-02-27 13:17:47 +0100

    Dont use labels.

--- django_q_prometheus/signals.py
+++ django_q_prometheus/signals.py
@@ -7,25 +7,26 @@ from django_q_prometheus.metrics import Metrics
 logger = logging.getLogger('django.server')
 
 MODE='livemostrecent'
+LABELS = []
 
 TASKS_SUCCESS = Gauge('django_q_tasks_success', 
-    'A count of successful tasks.', multiprocess_mode=MODE)
+    'A count of successful tasks.', LABELS, multiprocess_mode=MODE)
 TASKS_FAILED = Gauge('django_q_tasks_failed',
-    'A count of failed tasks.', multiprocess_mode=MODE)
+    'A count of failed tasks.', LABELS, multiprocess_mode=MODE)
 TASKS_QUEUED = Gauge('django_q_tasks_queued',
-    'A count of queued tasks.', multiprocess_mode=MODE)
+    'A count of queued tasks.', LABELS, multiprocess_mode=MODE)
 SCHEDULES_COUNT = Gauge('django_q_schedules_count', 
-    'A count of schedules.', multiprocess_mode=MODE)
+    'A count of schedules.', LABELS, multiprocess_mode=MODE)
 CLUSTER_COUNT = Gauge('django_q_cluster_count',
-    'A count of clsuters.', multiprocess_mode=MODE)
+    'A count of clsuters.', LABELS, multiprocess_mode=MODE)
 WORKER_COUNT = Gauge('django_q_worker_count',
-    'A count of workers.', multiprocess_mode=MODE)
+    'A count of workers.', LABELS, multiprocess_mode=MODE)
 REINCARNATION_COUNT = Gauge('django_q_reincarnation_count', 
-    'A count of processes that have been reincarnated.', multiprocess_mode=MODE)
+    'A count of processes that have been reincarnated.', LABELS, multiprocess_mode=MODE)
 AVERAGE_EXEC_TIME = Gauge('django_q_average_execution_seconds',
-    'The average execution time in the last 24 hours.', multiprocess_mode=MODE)
+    'The average execution time in the last 24 hours.', LABELS, multiprocess_mode=MODE)
 TASKS_SUCCESS_PER_DAY = Gauge('django_q_tasks_per_day',
-    'The count of sucessful tasks in the last 24 hours', multiprocess_mode=MODE)
+    'The count of sucessful tasks in the last 24 hours', LABELS, multiprocess_mode=MODE)
 
 def call_hook(sender, **kwargs):
     try:
