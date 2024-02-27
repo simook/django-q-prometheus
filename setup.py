03d6357c3792b4b8e761ddac453d3d1d8febf61e
Kyle Simukka <simook@gmail.com>
2024-02-27 09:09:08 +0100

    Fixed the package naming.

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='django-q-prometheus',
-    packages=['django-q-prometheus'],
+    packages=['django_q_prometheus'],
     description='django-q performance metrics',
     version='1.0.0',
-    url='http://github.com/simooka/django-q-prometheus',
+    url='http://github.com/simook/django-q-prometheus',
     author='Kyle Simukka',
     author_email='kylesimukka@gmail.com',
     keywords=['django-q','prometheus','metrics'],
